d979c7e011211a306e2de50533c30439d2b2d0ae
Jacob <Jacobemartinez2000@gmail.com>
2025-03-02 22:33:48 -0700

    MemoryAuthDAO

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,12 +1,11 @@
 package dataaccess;
 
 import model.AuthData;
-import java.util.List;
 
 public interface AuthDAO {
     void createAuth(AuthData auth) throws DataAccessException;
     AuthData getAuth(String authToken) throws DataAccessException;
     void updateAuth(AuthData auth) throws DataAccessException;
     void deleteAuth(String authToken) throws DataAccessException;
-    List<AuthData> getAllAuths() throws DataAccessException;
+    void clearAllAuths() throws DataAccessException;
 }
---
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -0,0 +1,52 @@
+package dataaccess;
+
+import model.AuthData;
+import java.util.Map;
+import java.util.HashMap;
+
+public class MemoryAuthDAO implements AuthDAO {
+
+    final private Map<String, AuthData> auths = new HashMap<>();
+
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException {
+
+        if(auths.containsKey(auth.authToken())){
+            throw new DataAccessException("AuthToken Taken");
+        }
+
+        AuthData myAuth = new AuthData(auth.authToken(), auth.username());
+        auths.put(auth.authToken(), myAuth);
+    }
+
+    @Override
+	public AuthData getAuth(String authToken) throws DataAccessException {
+        if(!auths.containsKey(authToken)){
+            throw new DataAccessException("Auth Token Does Not Exist");
+        }
+        return auths.get(authToken);
+
+    }
+
+    @Override
+    public void updateAuth(AuthData auth) throws DataAccessException {
+        if (!auths.containsKey(auth.authToken())) {
+            throw new DataAccessException("Auth Token Does Not Exist");
+        }
+        auths.put(auth.authToken(), new AuthData(auth.authToken(), auth.username()));
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        if (!auths.containsKey(authToken)) {
+            throw new DataAccessException("Auth Token Does Not Exist");
+        }
+        auths.remove(authToken);
+    }
+
+    @Override
+	public void clearAllAuths() {
+        auths.clear();
+    }
+}
+
