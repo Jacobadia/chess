936105e9ff21a0080ac73896adc6c452b20737ef
Jacob <Jacobemartinez2000@gmail.com>
2025-03-25 13:30:22 -0600

    Repl

---
+++ client/src/main/java/ui/ReplMenu.java
@@ -0,0 +1,41 @@
+package ui;
+
+
+
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
+public class ReplMenu {
+	private final PreLogInClient client;
+
+	public ReplMenu(String serverUrl) {
+		client = new PreLogInClient(serverUrl);
+	}
+
+	public void run() {
+		System.out.println("Welcome to Chess. Please log in or register.");
+		System.out.print(client.help());
+
+		Scanner scanner = new Scanner(System.in);
+		var result = "";
+		while (!result.equals("quit")) {
+			printPrompt();
+			String line = scanner.nextLine();
+
+			try {
+				result = client.eval(line);
+				System.out.print(result);
+			} catch (Throwable e) {
+				var msg = e.toString();
+				System.out.print(msg);
+			}
+		}
+		System.out.println();
+	}
+
+	private void printPrompt() {
+		System.out.print("\n" + ">>> ");
+	}
+
+}
